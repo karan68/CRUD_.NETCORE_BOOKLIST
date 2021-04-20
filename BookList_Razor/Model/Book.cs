edcedf6dacb270ba9bf11ae6fdc0261616c6f1f1
karan yadav <47856276+karan68@users.noreply.github.com>
2021-04-20 22:14:55 +0530

    Add files via upload

---
+++ BookList_Razor/Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList_Razor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Book Name")]
+        public string Name { get; set; }
+
+        public string ISBN { get; set; }
+    
+        public String Author { get; set; }
+    }
+}
